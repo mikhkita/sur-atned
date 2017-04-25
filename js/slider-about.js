0240db1791bb7db24baa4aa075734556648cac24
Vitaliy Zhalnin <cueage@gmail.com>
2017-04-25 13:58:52 +0700

    поправил кнопки slick prev next

--- js/slider-about.js
+++ js/slider-about.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
 	$('.left-slider').slick({
 		asNavFor: '.right-slider',
-		nextArrow: '<div class="b-btn-next"></div>',
-		prevArrow: '<div class="b-btn-prev"></div>'
+		prevArrow: '<div class="b-btn-prev"></div>',
+		nextArrow: '<div class="b-btn-next"></div>'
+		
 	});
 	$('.right-slider').slick({
 		dots: true,
--- js/top-slider.js
+++ js/top-slider.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
 	$('.back-slider').slick({
 		dots: true,
-		asNavFor: '.front-slider'	
+		asNavFor: '.front-slider',
+		prevArrow:false,
+		nextArrow:false	
 	});
 	$('.front-slider').slick({
 		fade: true,
 		speed: 500,
 		fade: true,
 		cssEase: 'linear',
-		asNavFor: '.back-slider'
+		asNavFor: '.back-slider',
+		prevArrow:false,
+		nextArrow:false		
 	});
 });

bdc49d69719e32e37d7ad197e181bb97ee26a6b9
Vitaliy Zhalnin <cueage@gmail.com>
2017-04-28 19:52:31 +0700

    слайдер в ненужном месте под всеми кнопками…

---
+++ js/services_page_slider.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+	// $('.p_services_menu_info_slider').slick({
+	// 	customPaging: function (slider, i)
+	// 	 {
+ //               return i + 1;
+ //  },
+	// 	slidesToShow: 1,
+	// 	slidesToScroll: 1,
+	// 	dots: true,
+
+	// 	prevArrow:false,
+	// 	nextArrow:false	
+	// });
+$('.p_services_menu_info_slider').slick({
+       customPaging: function (slider, i)
+ {
+ 		if ( i == 0 ) {return '<a class="icon-diagnostic"> <span>Диагностика</span></a>';}
+ 		if ( i == 1 ) {return '<a class="icon-orthopedic"> <span>ортопедия</span></a>';}
+ 		if ( i == 2 ) {return '<a class="icon-orthodontic"> <span>Ортодонтия</span></a>';}
+ 		if ( i == 3 ) {return '<a class="icon-profgigiena"> <span>Профгигиена и отбеливание</span></a>';}
+ 		if ( i == 4 ) {return '<a class="icon-terapevt"> <span>Терапевтическая стоматология</span></a>';}
+ 		if ( i == 5 ) {return '<a class="icon-estetic"> <span>Эстетическая стоматология</span></a>';}
+ 		if ( i == 6 ) {return '<a class="icon-surgery"> <span>хирургия и имплантация</span></a>';}
+ 		if ( i == 7 ) {return '<a class="icon-childrens"> <span>детская стоматология</span></a>';}
+               
+  },
+       dots:true,
+		prevArrow:false,
+		nextArrow:false,	       
+       appendDots:$('.p_services_menu')
+   });	
+});
+
+

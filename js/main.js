02e483c3226d9ab84da8a5fb6d9f7fd3bf7bcc23
evox1994 <dmitrybatukhtin@gmail.com>
2017-05-08 21:02:09 +0700

    Оба попапа

--- js/main.js
+++ js/main.js
@@ -36,7 +36,13 @@ $(document).ready(function(){
         monthNames: [ "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" ],
         dateFormat: "d MM yy"
     });
-    $('#tel-2').mask("+7(999)999-99-99");	
+    $('#tel-2').mask("+7(999)999-99-99");
+    $('#tel-3').mask("+7(999)999-99-99");	
+    $(function() {
+          [].slice.call( document.querySelectorAll( 'select.cs-select' ) ).forEach( function(el) {   
+             new SelectFx(el);
+          } );
+        });
     function resize(){
        if( typeof( window.innerWidth ) == 'number' ) {
             myWidth = window.innerWidth;
@@ -113,156 +119,152 @@ $(document).ready(function(){
     // };
 
     // var jssor_slider1 = new $JssorSlider$("slider1_container", options);
-    var Morphing = function( $btn, opts ) {
+    $(function(){
+        var Morphing = function( $btn, opts ) {
 
-        this.opts = opts;
+            this.opts = opts;
 
-        this._init( $btn );
+            this._init( $btn );
 
-    }
+        }
 
-    Morphing.prototype._init = function( $btn ) {
-        var that = this;
+        Morphing.prototype._init = function( $btn ) {
+            var that = this;
 
-        that.$btn = $btn.width( $btn.width() ).addClass('morphing-btn');
+            that.$btn = $btn.width( $btn.width() ).addClass('morphing-btn');
 
-        // Add wrapping element and set initial width used for positioning
-        $btn.wrap(function() {
-            var $wrap = $('<div class="morphing-btn-wrap"></div>');
+            // Add wrapping element and set initial width used for positioning
+            $btn.wrap(function() {
+                var $wrap = $('<div class="morphing-btn-wrap"></div>');
 
-            $wrap.width( $(this).outerWidth( true ) );
+                $wrap.width( $(this).outerWidth( true ) );
 
-            return $wrap;
-        });
+                return $wrap;
+            });
 
-        that.$clone = $('<div />')
-            .hide()
-            .addClass('morphing-btn-clone')
-            .insertAfter( $btn );
+            that.$clone = $('<div />')
+                .hide()
+                .addClass('morphing-btn-clone')
+                .insertAfter( $btn );
 
-        $btn.on('click', function(e) {
-            e.preventDefault();
+            $btn.on('click', function(e) {
+                e.preventDefault();
 
-            that.open();
-        });
-    };
+                that.open();
+            });
+        };
 
-    Morphing.prototype.open = function() {
-      var that = this;
+        Morphing.prototype.open = function() {
+          var that = this;
 
-      if ( that.$btn.hasClass('morphing-btn_circle') ) {
-        return;
-      }
+          if ( that.$btn.hasClass('morphing-btn_circle') ) {
+            return;
+          }
 
-      // First, animate button to the circle
-        that.$btn.one("transitionend.fm webkitTransitionEnd.fm oTransitionEnd.fm MSTransitionEnd.fm", function(e) {
+          // First, animate button to the circle
+            that.$btn.one("transitionend.fm webkitTransitionEnd.fm oTransitionEnd.fm MSTransitionEnd.fm", function(e) {
 
-            // if ( e.originalEvent.propertyName !== 'width' ) {
-            //     return;
-            // }
+                // if ( e.originalEvent.propertyName !== 'width' ) {
+                //     return;
+                // }
 
-            $(this).off(".fm");
+                $(this).off(".fm");
 
-            that._animate();
+                that._animate();
 
-        });
+            });
 
-        that.$btn.width( that.$btn.width() ).addClass('morphing-btn_circle');
+            that.$btn.width( that.$btn.width() ).addClass('morphing-btn_circle');
 
-    };
+        };
 
-    Morphing.prototype._animate = function() {
-        var that   = this;
-        var $btn   = that.$btn;
-        var $clone = that.$clone;
-        var scale  = this._retrieveScale( $btn );
-        var pos    = $btn[0].getBoundingClientRect();
+        Morphing.prototype._animate = function() {
+            var that   = this;
+            var $btn   = that.$btn;
+            var $clone = that.$clone;
+            var scale  = this._retrieveScale( $btn );
+            var pos    = $btn[0].getBoundingClientRect();
 
-        $clone.css({
-            top       : pos.top  + $btn.outerHeight() * 0.5 - ( $btn.outerHeight() * scale * 0.5 ),
-            left      : pos.left + $btn.outerWidth()  * 0.5 - ( $btn.outerWidth()  * scale * 0.5 ),
-            width     : $btn.outerWidth()  * scale,
-            height    : $btn.outerHeight() * scale,
-            transform : 'scale(' + 1 / scale + ')'
-        });
+            $clone.css({
+                top       : pos.top  + $btn.outerHeight() * 0.5 - ( $btn.outerHeight() * scale * 0.5 ),
+                left      : pos.left + $btn.outerWidth()  * 0.5 - ( $btn.outerWidth()  * scale * 0.5 ),
+                width     : $btn.outerWidth()  * scale,
+                height    : $btn.outerHeight() * scale,
+                transform : 'scale(' + 1 / scale + ')'
+            });
 
-        $clone.one("transitionend.fm webkitTransitionEnd.fm oTransitionEnd.fm MSTransitionEnd.fm", function(e) {
-            $(this).off(".fm");
+            $clone.one("transitionend.fm webkitTransitionEnd.fm oTransitionEnd.fm MSTransitionEnd.fm", function(e) {
+                $(this).off(".fm");
 
-            // Open fancyBox
-            $.fancybox.open({ src : $btn.data('src') || $btn.attr('href') }, $.extend({}, that.opts, {
-                infobar  : false,
-                buttons  : false,
-                smallBtn : false,
-                touch    : false,
-                closeClickOutside : false,
-                modal : true,
-                onInit : function( instance ) {
-                    instance.$refs.slider_wrap.append('<button class="morphing-close" data-fancybox-close>X</button>');
-                    instance.$refs.bg.remove();
-                },
-                afterClose : function() {
-                    that.close();
+                // Open fancyBox
+                $.fancybox.open({ src : $btn.data('src') || $btn.attr('href') }, $.extend({}, that.opts, {
+                    infobar  : false,
+                    buttons  : false,
+                    smallBtn : false,
+                    touch    : false,
+                    closeClickOutside : false,
+                    modal : true,
+                    onInit : function( instance ) {
+                        instance.$refs.slider_wrap.append('<button class="morphing-close" data-fancybox-close>X</button>');
+                        instance.$refs.bg.remove();
+                    },
+                    afterClose : function() {
+                        that.close();
+                    }
+                }));
+
+            });
+
+            // Trigger expanding of the cloned element
+            $clone.show().addClass('morphing-btn-clone_visible');
+
+        };
+
+        Morphing.prototype.close = function() {
+            var that   = this;
+            var $btn   = that.$btn;
+            var $clone = that.$clone;
+            var scale  = that._retrieveScale( $btn );
+            var pos    = $btn[0].getBoundingClientRect();
+
+            $clone.css({
+                top       : pos.top  + $btn.outerHeight() * 0.5 -  ( $btn.outerHeight() * scale * 0.5 ),
+                left      : pos.left + $btn.outerWidth()  * 0.5  - ( $btn.outerWidth()  * scale * 0.5 ),
+                width     : $btn.outerWidth()  * scale,
+                height    : $btn.outerHeight() * scale,
+                transform : 'scale(' + ( 1 / scale ) + ')'
+            });
+
+            $clone.one('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function(e) {
+                $clone.hide();
+
+                $btn.removeClass('morphing-btn_circle');
+            });
+
+            $clone.removeClass('morphing-btn-clone_visible');
+        };
+
+        Morphing.prototype._retrieveScale = function( $btn ) {
+            var rez = Math.max( $(window).height() * 2 / $btn.height() , $(window).width() * 2 / $btn.width() );
+
+            return rez;
+        };
+
+        $.fn.fancyMorph = function( opts ) {
+            this.each(function() {
+                var $this = $(this);
+
+                if ( !$this.data('morphing') ) {
+                    $this.data('morphing', new Morphing( $this, opts ));
                 }
-            }));
 
+            });
+
+            return this;
+        };
+
+        $("[data-morphing]").fancyMorph({
+            hash : 'morphing'
         });
-
-        // Trigger expanding of the cloned element
-        $clone.show().addClass('morphing-btn-clone_visible');
-
-    };
-
-    Morphing.prototype.close = function() {
-        var that   = this;
-        var $btn   = that.$btn;
-        var $clone = that.$clone;
-        var scale  = that._retrieveScale( $btn );
-        var pos    = $btn[0].getBoundingClientRect();
-
-        $clone.css({
-            top       : pos.top  + $btn.outerHeight() * 0.5 -  ( $btn.outerHeight() * scale * 0.5 ),
-            left      : pos.left + $btn.outerWidth()  * 0.5  - ( $btn.outerWidth()  * scale * 0.5 ),
-            width     : $btn.outerWidth()  * scale,
-            height    : $btn.outerHeight() * scale,
-            transform : 'scale(' + ( 1 / scale ) + ')'
-        });
-
-        $clone.one('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function(e) {
-            $clone.hide();
-
-            $btn.removeClass('morphing-btn_circle');
-        });
-
-        $clone.removeClass('morphing-btn-clone_visible');
-    };
-
-    Morphing.prototype._retrieveScale = function( $btn ) {
-        var rez = Math.max( $(window).height() * 2 / $btn.height() , $(window).width() * 2 / $btn.width() );
-
-        return rez;
-    };
-
-    $.fn.fancyMorph = function( opts ) {
-        this.each(function() {
-            var $this = $(this);
-
-            if ( !$this.data('morphing') ) {
-                $this.data('morphing', new Morphing( $this, opts ));
-            }
-
-        });
-
-        return this;
-    };
-
-    $("[data-morphing]").fancyMorph({
-        hash : 'morphing'
     });
-
-    (function() {
-      [].slice.call( document.querySelectorAll( 'select.cs-select' ) ).forEach( function(el) {   
-         new SelectFx(el);
-      } );
-   })();
 });

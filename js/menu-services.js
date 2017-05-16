e2a1e83b28c051d685bcd57e4df1c1a877fe87d1
vitalo <cueage@gmail.com>
2017-05-16 17:56:40 +0700

    index page services buttons etc

--- js/menu-services.js
+++ js/menu-services.js
@@ -7,5 +7,5 @@ $('.services-image').addClass('hide').removeClass('show');
 $('.services-images [data-id ='+dataVal+']').addClass('show').removeClass('hide');
 $('.services-text-container').addClass('hide').removeClass('show');
 $('.services-texts [data-id ='+dataVal+']').addClass('show').removeClass('hide');
-  // alert('Вы нажали на элемент "foo"' + dataVal);
+
 });

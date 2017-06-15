fbc537ad945d4a3ac673cf5af929bc1cd7988608
evox1994 <dmitrybatukhtin@gmail.com>
2017-06-15 21:08:07 +0700

    Исправлен календарь

--- js/main.js
+++ js/main.js
@@ -1285,11 +1285,13 @@ $(function(){
         } else {
             if ( $(event.target).closest("a,tr,.ui-datepicker-title,.ui-datepicker-calendar").length ) return;
             $('.div-cal').removeClass("open-cal");
+            $('.ui-datepicker').removeClass("open-ui");
             event.stopPropagation();
         }
     });
     $('.date-div').click(function(){
         $('.div-cal').addClass("open-cal");
+        $('.ui-datepicker').addClass("open-ui");
     });
     $('.p_doctors_menu').on('afterChange',function(event,slick,currentSlide){
         var Morph = function( $btn, opts ) {
